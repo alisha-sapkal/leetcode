707a1923f419212bd73be38e2a660ef9f4861933
Alisha Sapkal <145154936+alisha-sapkal@users.noreply.github.com>
2025-03-03 17:24:23 +0530

    Time: 3 ms (19.05%) | Memory: 45.1 MB (50.38%) - LeetSync

---
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int maxSum = 0, minSum = 0, currMax = 0, currMin = 0;
+        for (int num : nums) {
+            currMax = max(num, currMax + num);
+            maxSum = max(maxSum, currMax);
+            currMin = min(num, currMin + num);
+            minSum = min(minSum, currMin);
+        }
+        return max(abs(maxSum), abs(minSum));
+    }
+};
